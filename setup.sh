85cab33943ae802eff3f787c45c0cef9112be8c1
Daniel Levenstein <danielalevenstein@gmail.com>
2026-06-12 21:17:18 -0500

    copy training data as part of setup.sh
    # Conflicts:
    #	setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 source .venv/bin/activate
+# Ensure the directory exists
+mkdir -p data/curated
+cp -f config/curated_training_data.json data/curated/curated_training_data.json
 pip3 install -e .
 bash generate_data.sh
